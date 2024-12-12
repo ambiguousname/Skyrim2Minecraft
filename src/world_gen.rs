2c3f4e2a8e104d862efa4914c786847f75999e3f
Tyler K <tyly04@gmail.com>
2024-12-12 05:33:06 -0800

    Adding utility functions for drawing

--- src/world_gen.rs
+++ src/world_gen.rs
@@ -19,20 +19,48 @@ pub struct BlockState {
 }
 
 impl BlockState {
-	pub fn new_from_palette(palette : Vec<Block>, heights : Vec<i8>) -> BlockState {
+	pub fn new_from_palette(palette : Vec<Block>) -> BlockState {
 		if palette.len() >= 16 {
 			panic!("Palettes of length 16 or greater are not currently supported.");
 		}
 
-		let mut dat = Vec::with_capacity(256);
-
-		for h in heights {
-			
-		}
+		let dat = vec![0; 256];
 
 		BlockState {
 			palette,
 			data: Some(dat)
+		}
+	}
+
+	pub fn draw_height(&mut self, idx : i8, x : usize, z : usize, start_y : usize, end_y : usize) {
+		let dat = self.data.as_mut().unwrap();
+
+		let z_shift = (z % 4) * 4;
+
+		for y in start_y..(end_y + 1) {
+			let xy_idx = x + (y * 16);
+			let curr = dat[xy_idx];
+
+			let zeroed = curr & !(0b1111 << z_shift);
+			dat[xy_idx] = zeroed | ((idx as i64 & 0b1111) << z_shift);
+			println!("{xy_idx} {z_shift} {} {}", curr, dat[xy_idx]);
+		}
+	}
+
+	pub fn fill_layer(&mut self, idx : i8, y : usize) {
+		let dat = self.data.as_mut().unwrap();
+
+		let idx_64 = idx as i64;
+
+		let mut full = 0;
+
+		for i in 0..16 {
+			full |= idx_64 << (i * 4);
+		}
+
+		for x in 0..16 {
+			let xy_idx = x + (y * 16);
+			dat[xy_idx] = full;
 		}
 	}
 }
@@ -77,6 +105,18 @@ const MIN_Y : i32 = -1024;
 
 impl Default for Chunk {
 	fn default() -> Self {
+		let mut bottom_block = BlockState::new_from_palette(vec![
+			Block { 
+				name: "minecraft:air".into(),
+				properties: HashMap::new() 
+			},
+			Block{
+				name: "minecraft:bedrock".into(),
+				properties: HashMap::new()
+			}
+		]);
+		bottom_block.fill_layer(1, 0);
+		
 		Self {
 			data_version: 4189,
 
@@ -89,17 +129,7 @@ impl Default for Chunk {
 			sections: vec![
 				Section {
 					y: (MIN_Y >> 4) as i8,
-					block_states: BlockState {
-						palette: vec![Block{
-							name: "minecraft:bedrock".into(),
-							properties: HashMap::new()
-						},
-						Block { 
-							name: "minecraft:air".into(),
-							properties: HashMap::new() 
-						}],
-						data: Some(vec![0 ; 256])
-					},
+					block_states: bottom_block,
 					biomes: Biomes {
 						palette: vec!["minecraft:plains".into()],
 					}
